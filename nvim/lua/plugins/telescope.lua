7a893262b3004e110763cc5db1ee742789c7cc75
mkey7 <houjiang123456@sina.com>
2024-11-22 11:23:28 +0800

    telescope 搜索 隐藏文件

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,13 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Telescope",
-    keys = {
-      { "<leader>f",  ":Telescope find_files<CR>", desc = "find files" },
-      { "<leader>F",  ":Telescope live_grep<CR>",  desc = "grep file" },
-      { "<leader>rs", ":Telescope resume<CR>",     desc = "resume" },
-      { "<C-q>",      ":Telescope oldfiles<CR>",   desc = "oldfiles" },
-    },
-
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>f", ":Telescope find_files hidden=true<CR>", desc = "find files" },
+			{ "<leader>F", ":Telescope live_grep<CR>", desc = "grep file" },
+			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
+			{ "<C-q>", ":Telescope oldfiles<CR>", desc = "oldfiles" },
+		},
+	},
 }
